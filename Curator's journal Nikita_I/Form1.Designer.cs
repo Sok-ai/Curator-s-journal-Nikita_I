dd46992ae3fb173e4c24606b6d1a0f221ce97ab0
Samurai <sokol152433@gmail.com>
2022-05-05 16:46:00 +0300

    Добавьте файлы проекта.

---
+++ Curator's journal Nikita_I/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Curator_s_journal_Nikita_I
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(0, 0);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(228, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(228, 68);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(225, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Первый дежурный";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(225, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Второй дежурный";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(228, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 39);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(229, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 39);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Сформировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 320);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(222, 139);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(497, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Н/Б";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(651, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Б";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Curator's journal Nikita_I/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Curator_s_journal_Nikita_I
+{
+    public partial class Form1 : Form
+    {
+        string[] students = {"Авилов",
+                            "Бакашвили" ,
+                            "Бирюков" ,
+                            "Борисов" ,
+                            "Бочаров" ,
+                            "Вандышев" ,
+                            "Гладченко" ,
+                            "Гузовский" ,
+                            "Еременко" ,
+                            "Ищенко" ,
+                            "Московкин" ,
+                            };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            monthCalendar1.MaxDate = DateTime.Today;
+
+            int pos = 18;
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                Label label = new Label();
+                label.Name = "stlabel" + i;
+                label.AutoSize = true;
+                label.Font = new Font(label.Font.Name, 9, label.Font.Style);
+                label.Text = students[i];
+                label.Location = new Point(300, pos + 5);
+                this.Controls.Add(label);
+
+                CheckBox checkBox1 = new CheckBox();
+                checkBox1.Name = "stCheckBox" + i;
+                checkBox1.Location = new Point(380, pos);
+                this.Controls.Add(checkBox1);
+
+                CheckBox checkBox2 = new CheckBox();
+                checkBox2.Name = "stCheckBox" + i;
+                checkBox2.Location = new Point(490, pos);
+                this.Controls.Add(checkBox2);
+
+                pos += 28;
+            }
+            comboBox1.Items.AddRange(students);
+            comboBox2.Items.AddRange(students);
+        }
+        List<List<string>> data = new List<List<string>>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool dateExists = false;
+            string datePatt = @"dd.MM.yyyy";
+
+            foreach (var cell in data)
+            {
+                if (cell[0] == monthCalendar1.SelectionStart.ToString(datePatt))
+                {
+                    dateExists = MessageBox.Show("Такая запись уже существует, добавить ещё одну?", "Внимание", MessageBoxButtons.YesNo) == DialogResult.Yes ? false : true;
+                }
+
+            }
+            if (!dateExists)
+            {
+                List<string> addedData = new List<string>();
+                addedData.Add(monthCalendar1.SelectionStart.ToString(datePatt));
+                for (int i = 0; i < students.Length; i++)
+                {
+                    CheckBox checkBox = (CheckBox)this.Controls["stCheckBox" + i];
+                    addedData.Add((checkBox.Checked) ? "Н/б" : " ");
+                 
+                }
+                addedData[comboBox1.SelectedIndex + 1] = (addedData[comboBox1.SelectedIndex + 1] == " ") ? "Д" : addedData[comboBox1.SelectedIndex + 1] + ", Д";
+                addedData[comboBox2.SelectedIndex + 1] = (addedData[comboBox2.SelectedIndex + 1] == " ") ? "Д" : addedData[comboBox2.SelectedIndex + 1] + ", Д";
+
+                data.Add(addedData);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var applicatoin = new Excel.Application();
+            applicatoin.SheetsInNewWorkbook = 1;
+            Excel.Workbook workbook = applicatoin.Workbooks.Add(System.Type.Missing);
+            Excel.Worksheet worksheet = applicatoin.Worksheets.Item[1];
+            worksheet.Name = "Посещаемость";
+
+            for (int k = 0; k < students.Length; k++)
+            {
+                worksheet.Cells[1][k + 2] = students[k];
+            }
+
+            int i = 2;
+            foreach (var cell in data)
+            {
+                int j = 1;
+                foreach (var row in cell)
+                {
+                    worksheet.Cells[i][j] = row;
+                    j++;
+                }
+                i++;
+            }
+            applicatoin.Visible = true;
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            string datePatt = @"dd.MM.yyyy";
+            bool dateExists = false;
+            comboBox1.SelectedIndex = -1;
+            comboBox2.SelectedIndex = -1;
+
+            foreach (var cell in data)
+            {
+                if (cell[0] == monthCalendar1.SelectionStart.ToString(datePatt))
+                {
+                    for (int i = 0; i < students.Length; i++)
+                    {
+                        CheckBox checkBox = (CheckBox)this.Controls["stCheckBox" + i];
+                        checkBox.Checked = (cell[i + 1] == " " || cell[i + 1] == "Д") ? true : false;
+                        if (comboBox1.SelectedIndex == -1)
+                            comboBox1.SelectedIndex = (cell[i + 1] == "Д" || cell[i + 1] == "Н/б, Д") ? i : -1;
+                        if (comboBox2.SelectedIndex == -1)
+                            comboBox2.SelectedIndex = (cell[i + 1] == "Д" || cell[i + 1] == "Н/б, Д") ? i : -1;
+                        dateExists = true;
+                    }
+                }
+            }
+            if (!dateExists)
+            {
+                for (int i = 0; i < students.Length; i++)
+                {
+                    CheckBox checkBox = (CheckBox)this.Controls["stCheckBox" + i];
+                    checkBox.Checked = false;
+                }
+                comboBox1.SelectedIndex = -1;
+                comboBox2.SelectedIndex = -1;
+            }
+        }
+    }
+}
